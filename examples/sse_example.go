e34c44875191c0af2dd8449edc3906db8ab1d5ce
NeoMitashi <jagannthkn@gmail.com>
2023-08-11 18:35:20 +0530

    examples

--- examples/sse_example.go
+++ examples/sse_example.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/duoxehyon/mev-share-go/sse"
+)
+
+func main() {
+	client := sse.New("https://mev-share.flashbots.net")
+
+	eventChan := make(chan sse.Event)
+	sub, err := client.Subscribe(eventChan)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		event := <-eventChan
+		if event.Error != nil {
+			fmt.Println("Error occured: ", event.Error)
+		}
+
+		fmt.Println(event)
+
+		sub.Stop()
+		break
+	}
+}
